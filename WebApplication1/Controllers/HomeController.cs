14ca95c1fad856a5db9aeaa8caec019e14294a50
For <For@DESKTOP-9UASVKR>
2023-12-16 07:08:05 +0300

    7:07

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication1.Controllers
+{
+    public class Home : Controller
+    {
+
+        public IActionResult Index()
+        {
+            ViewData["Text"] = "Лабораторная работа 2";
+            return View();
+        }
+
+        public class ListDemo
+        {
+            public int ListItemValue { get; set; }
+            public string ListItemText { get; set; }
+        }
+        private List<ListDemo> _listDemo;
+
+
+
+        public Home(List<ListDemo> listDemo)
+        {
+            _listDemo = listDemo;
+
+        }
+    }
+}
